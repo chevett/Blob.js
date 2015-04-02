5e280c3c7226fb3c7b9185d0bbaab839aaa3201a
Mike Chevett <mchevett@gmail.com>
2015-04-02 11:26:26 -0400

    try without the blob

---
+++ data-uri-to-array.js
@@ -0,0 +1,11 @@
+module.exports = function(dataURI){
+	var u = dataURI.split(',')[1],
+    	binary = atob(u),
+    	array = [];
+
+    for (var i = 0; i < binary.length; i++) {
+        array.push(binary.charCodeAt(i));
+    }
+
+	return new Uint8Array(array);
+};
